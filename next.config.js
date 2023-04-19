f29c45eece7abaf8b6daa3f44f34b1f056218f59
Niels Mündler <n.muendler@posteo.de>
2023-04-19 03:09:51 +0200

    More opshin!

--- next.config.js
+++ next.config.js
@@ -1,30 +1,11 @@
 /** @type { import("nextra").Nextra } */
 const nextra = require("nextra");
-const { getHighlighter, BUNDLED_LANGUAGES } = require("shiki");
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
   readingTime: true,
-  mdxOptions: {
-    rehypePrettyCodeOptions: {
-      getHighlighter: (options) => {
-        return getHighlighter({
-          ...options,
-          langs: [
-            ...BUNDLED_LANGUAGES,
-            {
-              id: "aiken",
-              scopeName: "source.aiken",
-              grammar: require("./aiken.tmLanguage.json"),
-              aliases: ["ak", "aiken"],
-            },
-          ],
-        });
-      },
-    },
-  },
 });
 
 module.exports = withNextra({
